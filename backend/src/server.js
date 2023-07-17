c962e306f6daf992f050f2738e6945c7449c1d9a
sripriya <sripriya@twilightsoftwares.com>
2023-07-17 17:15:50 +0530

    fix:testpush

--- backend/src/server.js
+++ backend/src/server.js
@@ -26,7 +26,7 @@ app.use('/api/v1', userRoutes);
 sequelize
   .sync({ logging: false })
   .then(() => {
-    logger.info('DB Connection Successfully testing 1');
+    logger.info('DB Connection Successfully testing 12');
     app.listen(config.app.port, () => {
       logger.info(`Listening to port ${config.app.port}`);
     });
