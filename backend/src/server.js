d7f19aba4c6612705130d3e9c47a9336b94101b3
sripriya <sripriya@twilightsoftwares.com>
2023-07-26 16:13:07 +0530

    fix:test111

--- backend/src/server.js
+++ backend/src/server.js
@@ -26,7 +26,7 @@ app.use('/api/v1', userRoutes);
 sequelize
   .sync({ logging: false })
   .then(() => {
-    logger.info('DB Connection Successfully testing hook3');
+    logger.info('DB Connection Successfully testing hook43');
     app.listen(config.app.port, () => {
       logger.info(`Listening to port ${config.app.port}`);
     });
